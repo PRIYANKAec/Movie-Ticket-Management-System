a897800d94368d5ad6fdf156f1791c07e1808bc0
PRIYANKAec <priyankaa261103@gmail.com>
2024-07-27 06:11:51 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Login from './components/login'
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div>
+    <Login />
+    <Button>Click me</Button>
+    </div>
+    
+  )
+}
+
+export default App
